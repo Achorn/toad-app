3368b26f32fbeaa17583dead3eb6c285f1fce95e
achorn <joshuaachorn@gmail.com>
2024-01-26 11:10:30 -0800

    cleaned up un-needed files and imports. added react router and sample routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>navbar placeholder</div>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/sample" element={<div>Sample</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
