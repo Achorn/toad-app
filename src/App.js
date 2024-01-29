469eaab5c0dba1d0ddadc5c91ae9a3c5bb952900
achorn <joshuaachorn@gmail.com>
2024-01-29 10:43:46 -0800

    testing out hash router for toad app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { ToadGame } from "./pages/ToadGame";
 import { Navbar } from "./components/Navbar";
 import { About } from "./pages/About";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<ToadGame />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
