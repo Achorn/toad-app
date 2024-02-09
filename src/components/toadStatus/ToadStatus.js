a04dc8232390c15d45789a2bd8ca801f13fda34c
achorn <joshuaachorn@gmail.com>
2024-02-09 10:00:05 -0800

    added color and width to toad status

--- src/components/toadStatus/ToadStatus.js
+++ src/components/toadStatus/ToadStatus.js
@@ -9,23 +9,18 @@ export const ToadStatus = () => {
     <div id="health-display" className="health-full">
       <h2 id="health-display-title">{toad.name}</h2>
 
-      <StatusBar title="water" />
-      <StatusBar title="education" />
+      <StatusBar title="water" date={Date.parse(toad.last_feeding)} />
+      <StatusBar title="education" date={Date.parse(toad.last_lesson)} />
     </div>
   );
 };
 
 const StatusBar = ({ title, date }) => {
-  const [width, setWidth] = useState("50%");
-  const [backgroundColor, setBackgroundColor] = useState("orange");
+  const [width, setWidth] = useState("0%");
+  const [backgroundColor, setBackgroundColor] = useState("red");
 
-  const updateBarPercentage = (barPercentage, stat) => {
-    let percentage = this.calculatePercentage(stat);
-    barPercentage.style.width = percentage + "%";
-    barPercentage.style.backgroundColor = "green";
-    barPercentage.style.backgroundColor = this.getBarColor(percentage);
-  };
   const calculatePercentage = (stat) => {
+    console.log("stat");
     const timeDifferenceMS = Date.now() - stat;
     const timeDifferenceHours = Math.floor(timeDifferenceMS / 3600000);
     if (timeDifferenceHours > 36) return 0;
@@ -36,12 +31,18 @@ const StatusBar = ({ title, date }) => {
     if (number > 25) return "orange";
     return "red";
   };
-  useEffect(() => {}, []);
-  // percentage
+  useEffect(() => {
+    console.log("data: ", date);
 
-  // color
-  //red blue green
-  // width
+    const updateBarPercentage = (stat) => {
+      let percentage = calculatePercentage(stat);
+      setBackgroundColor(getBarColor(percentage));
+      setWidth(percentage + "%");
+    };
+
+    updateBarPercentage(date);
+  }, [date]);
+
   return (
     <div id="data-container">
       <div className="data-display">
@@ -50,7 +51,7 @@ const StatusBar = ({ title, date }) => {
           <div
             id={`${title}-percentage`}
             className="bar-percentage"
-            style={{ width, background: backgroundColor }}
+            style={{ width: width, background: backgroundColor }}
           ></div>
           <div className="bar-percentage"></div>
         </div>
